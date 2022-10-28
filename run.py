921bcdbbfcee2bf614d151eeba243af7b09b3de6
DarraghColeman <darragh.coleman1@gmail.com>
2022-10-28 11:29:48 +0000

    Add hit and miss markers, add game end messages

--- run.py
+++ run.py
@@ -5,10 +5,11 @@
 
 from random import randint
 
-hidden_board = [[''] * 8 for x in range(8)]
-guess_board = [[''] * 8 for x in range(8)]
+hidden_board = [[' '] * 8 for x in range(8)]
+guess_board = [[' '] * 8 for x in range(8)]
 
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, }
+
 
 def print_board(board):
     print('   A B C D E F G H')
@@ -18,6 +19,7 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+
 def create_ships(board):
     for ship in range(5):
         ship_row, ship_column = randint(0,7), randint(0,7)
@@ -25,16 +27,18 @@ def create_ships(board):
             ship_row, ship_column = randint(0,7), randint(0,7)
         board[ship_row][ship_column] = 'X'
 
+
 def get_ship_location():
-    row = input('Please enter a ship row 1-8')
+    row = input('Please enter a ship row 1-8: ')
     while row not in '12345678':
         print('Please enter a valid row')
-        row = input('Please enter a ship row 1-8')
-    column = input('Please enter a ship column A-H').upper()
+        row = input('Please enter a ship row 1-8: ')
+    column = input('Please enter a ship column A-H: ').upper()
     while column not in 'ABCDEFGH':
         print('Please enter a valid column')
-        column = input('Please enter a ship column A-H').upper()
+        column = input('Please enter a ship column A-H: ').upper()
     return int(row) - 1, letters_to_numbers[column]
+
 
 def count_hit_ships(board):
     count = 0
@@ -44,7 +48,9 @@ def count_hit_ships(board):
                 count += 1
     return count
 
+
 create_ships(hidden_board)
+print_board(hidden_board)
 turns = 10
 while turns > 0:
     print('Welcome to Battleships')
