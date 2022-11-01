dc79bfb93f465d0257ab69c3885fbbb398b74abf
DarraghColeman <darragh.coleman1@gmail.com>
2022-11-01 20:35:36 +0000

    Remove half board for better player chances

--- run.py
+++ run.py
@@ -5,18 +5,18 @@
 
 from random import randint
 
-hidden_board = [[' '] * 8 for x in range(8)]
-guess_board = [[' '] * 8 for x in range(8)]
+hidden_board = [[' '] * 5 for x in range(5)]
+guess_board = [[' '] * 5 for x in range(5)]
 
-letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, }
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, }
 
 
 def print_board(board):
     """
     Game board layout
     """
-    print('  A B C D E F G H')
-    print(' -----------------')
+    print('  A B C D E')
+    print(' ----------')
     row_number = 1
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
@@ -28,9 +28,9 @@ def create_ships(board):
     Adds computer's random ship locations.
     """
     for ship in range(5):
-        ship_row, ship_column = randint(0,7), randint(0,7)
+        ship_row, ship_column = randint(0,4), randint(0,4)
         while board[ship_row][ship_column] == 'X':
-            ship_row, ship_column = randint(0,7), randint(0,7)
+            ship_row, ship_column = randint(0,4), randint(0,4)
         board[ship_row][ship_column] = 'X'
 
 
@@ -38,14 +38,14 @@ def get_ship_location():
     """
     Adds user input and error messages when user inputs incorrect data.
     """
-    row = input('Please enter a ship row 1-8: ')
-    while row not in '12345678':
+    row = input('Please enter a ship row 1-5: ')
+    while row not in '12345':
         print('Please enter a valid row')
-        row = input('Please enter a ship row 1-8: ')
-    column = input('Please enter a ship column A-H: ').upper()
-    while column not in 'ABCDEFGH':
+        row = input('Please enter a ship row 1-5: ')
+    column = input('Please enter a ship column A-E: ').upper()
+    while column not in 'ABCDE':
         print('Please enter a valid column')
-        column = input('Please enter a ship column A-H: ').upper()
+        column = input('Please enter a ship column A-E: ').upper()
     return int(row) - 1, letters_to_numbers[column]
 
 
